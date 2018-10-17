fdd894ba509508f5a1891660405bd207803de950
Rui <sprucewang@163.com>
2018-10-17 17:11:00 +0800

    add granfana

---
+++ fabfile.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+from __future__ import with_statement
+from fabric.api import *
+import time
+
+env.hosts = ['139.199.24.137']
+env.user = 'root'
+
+base_path = '/root/project/kubernetes-prometheus-monitoring'
+
+def deploy():
+    local('git push origin master ')
+
+    with cd(base_path):
+        run('pwd')
+        run('git pull')
+        run('kubectl delete -f manifests-all.yaml')
+        run('./build.sh')
+        run('kubectl create -f manifests-all.yaml')
